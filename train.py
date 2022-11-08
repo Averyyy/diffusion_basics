560d36bffd8a9a6eb6d1e12792302c238a52a72d
Averyyy <58547284+Averyyy@users.noreply.github.com>
2022-11-08 22:48:27 +0800

    train params

--- train.py
+++ train.py
@@ -7,7 +7,7 @@ model = Unet(
 
 diffusion = GaussianDiffusion(
     model,
-    image_size=128,
+    image_size=256,
     timesteps=1000,           # number of steps
     # number of sampling timesteps (using ddim for faster inference [see citation for ddim paper])
     sampling_timesteps=250,
@@ -17,9 +17,9 @@ diffusion = GaussianDiffusion(
 trainer = Trainer(
     diffusion,
     'dataset',
-    train_batch_size=32,
+    train_batch_size=2,
     train_lr=8e-5,
-    train_num_steps=700000,         # total training steps
+    train_num_steps=7000,         # total training steps
     gradient_accumulate_every=2,    # gradient accumulation steps
     ema_decay=0.995,                # exponential moving average decay
     amp=True                        # turn on mixed precision
